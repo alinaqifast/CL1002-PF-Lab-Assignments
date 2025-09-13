fccca8eff4d60aa1da9bd7d9c8d510ed2a606a23
Ali Naqi <k250989@nu.edu.pk>
2025-09-13 18:51:04 +0500

    Update Task 7 Password Check.c

--- LAB 04 Tasks/Task 7 Password Check.c
+++ LAB 04 Tasks/Task 7 Password Check.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<string.h>
-int main() {
-    char pass[20];
-    printf("Enter password: ");
-    scanf("%s", pass);
+#include <stdio.h>
 
-    if (strcmp(pass, "1234") == 0)
-        printf("Access Granted\n");
-    else
-        printf("Access Denied\n");
+int main() {
+    int pass;
+    printf("enter your password: ");
+    scanf("%d", &pass);
+    if (pass == 1234) {
+        printf("Access Granted");
+    } else {
+        printf("Access Denied");
+    }
     return 0;
 }
